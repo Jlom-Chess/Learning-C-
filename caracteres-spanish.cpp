201cf23642e8fcd60712fd5e5d8aa1c983505a64
Jlom-Chess <jlojeda48@gmail.com>
2022-02-22 20:56:41 -0500

    Una prueba más de uso de caracteres españoles en c++, no funciona bien

--- caracteres-spanish.cpp
+++ caracteres-spanish.cpp
@@ -1,26 +1,24 @@
-#include <stdio.h>
-// Añade paquete de idiomas
+#include <iostream>
 #include <locale.h>
 #include <windows.h>
+using namespace std;
 
-int main(void)
+int main()
 {
     // Declaración de variables
     char cadena[254];
-
-    // Establecer el idioma a español
-    //setlocale(LC_ALL, "es-ES.UTF-8"); // Cambiar locale - Suficiente para máquinas Linux
-    setlocale(LC_ALL, "es_ES.UTF8");
-    //SetConsoleCP(1252);           // Cambiar STDIN -  Para máquinas Windows
-    // SetConsoleOutputCP(1252);     // Cambiar STDOUT - Para máquinas Windows
+    //setlocale(LC_ALL, "spanish.utf8");
+    //SetConsoleOutputCP(850);
+    //SetConsoleCP(850);
 
     // Mostrar correctamente los caracteres especiales del español
-    printf("¡Éxito!. Se muestran los caracteres especiales del español.");
+    cout << "¡Éxito!. Se muestran los caracteres especiales del español.";
 
-    printf("Introduce un string con caracteres especiales: ");
+    cout << "Introduce un string con caracteres especiales: ";
     gets(cadena);
 
-    printf("El string con caracteres especiales que has introducido es: %s", cadena);
+    cout << "El string con caracteres especiales que has introducido es: ";
+    cout << cadena;
 
     return 0;
 }
